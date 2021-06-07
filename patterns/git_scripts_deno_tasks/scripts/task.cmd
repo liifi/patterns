fe6272976d06d68c79e5d1e979421abe43cc2aa5
Julio Stanley <code@juliostanley.com>
2021-06-07 08:19:23 -0400

    Updating patterns

--- patterns/git_scripts_deno_tasks/scripts/task.cmd
+++ patterns/git_scripts_deno_tasks/scripts/task.cmd
@@ -2,4 +2,4 @@
 set SCRIPT_DIR_WIN=%~dp0
 set SCRIPT_DIR=%SCRIPT_DIR_WIN:\=/%
 git-bash -c "bash_exe=$(where bash | head -n1); echo -n @\\""$bash_exe\\"" %%* > '%SCRIPT_DIR%/bash.cmd';"
-@"%SCRIPT_DIR%\bash.cmd" -c "'%SCRIPT_DIR%/%~n0' %*"
+@"%SCRIPT_DIR%\bash.cmd" -c "export PATH=/usr/bin:$PATH && '%SCRIPT_DIR%/%~n0' %*"
