d8550799002e7007671c03425335d0c077d89f1b
Julio Stanley <code@juliostanley.com>
2021-06-07 08:31:00 -0400

    Updating a pattern with info

--- patterns/git_scripts_deno_tasks/scripts/tasks/build.js
+++ patterns/git_scripts_deno_tasks/scripts/tasks/build.js
@@ -8,3 +8,7 @@ const tell_me = prompt('Tell me something:')
 console.log(`You told me: ${tell_me}`)
 // await $(`which echo`)
 await $(`echo Echoing what you told me: ${tell_me}`)
+
+console.log('If you ran "./scripts/task deps" these versions should match')
+await $(`terraform version`)
+await $(`kubectl version`)
